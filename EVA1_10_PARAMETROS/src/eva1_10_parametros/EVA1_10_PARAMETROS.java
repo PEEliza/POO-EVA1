e4d2947a86a62e4bc77bffa3e5a17bf3786b28c2
Poleth Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2023-02-21 19:37:22 -0600

    Add files via upload

---
+++ EVA1_10_PARAMETROS/src/eva1_10_parametros/EVA1_10_PARAMETROS.java
@@ -0,0 +1,17 @@
+
+package eva1_10_parametros;
+
+public class EVA1_10_PARAMETROS {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    nPrintln("HOLA");
+    }
+    public static void nPrintln(String message, int n){
+        for (int i = 0; i < n; i++) {
+            System.out.println(message);  
+        }
+     
+    }
+}
