dcd9d84b4064c41c5d2539780474edc4b5b39956
Poleth Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2023-02-21 19:35:00 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,14 @@
+
+package eva1_7_metodos_1;
+// AUTOR: POLETH E. ESCARCEGA LOPEZ
+public class EVA1_7_METODOS_1 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    myMethod(); //Invocar al método: llamada función         
+    }
+  public static void myMethod(){
+      System.out.println("You have called me: my name is myMethod!!");
+  }  
+}
