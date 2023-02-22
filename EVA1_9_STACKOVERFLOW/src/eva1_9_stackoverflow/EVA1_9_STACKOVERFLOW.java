3f73e9ac813fcc50c51c9ca637769c2fb481e053
Poleth Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2023-02-21 19:36:28 -0600

    Add files via upload

---
+++ EVA1_9_STACKOVERFLOW/src/eva1_9_stackoverflow/EVA1_9_STACKOVERFLOW.java
@@ -0,0 +1,15 @@
+
+package eva1_9_stackoverflow;
+// AUTOR: POLETH E. ESCARCEGA LOPEZ
+
+public class EVA1_9_STACKOVERFLOW {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+   A();
+    }
+    public static void A(){
+        A();
+    }
+}
