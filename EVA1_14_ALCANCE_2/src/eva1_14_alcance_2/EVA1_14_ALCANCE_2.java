317310b1b6ef37cc1655362d2e9f4d459d183353
Poleth Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2023-02-23 09:46:30 -0600

    Add files via upload

---
+++ EVA1_14_ALCANCE_2/src/eva1_14_alcance_2/EVA1_14_ALCANCE_2.java
@@ -0,0 +1,24 @@
+
+package eva1_14_alcance_2;
+// AUTOR: POLETH E. ESCARCEGA LOPEZ
+public class EVA1_14_ALCANCE_2 {
+
+    
+    public static void main(String[] args) {
+        int x = 100; // Visible dentro de todo el bloque main() 
+        
+        for (int i = 0; i < 10; i++) { // EMPIEZA EL FOR
+         int x = 100; // La variable esta declarada dentro del main y es
+                      // visible dentro del for
+           
+        int y = 100; 
+         if (i == 5){
+             
+         }   
+        } // TERMINA EL FOR
+    int y = 100; // Aquí la variable y del ciclo for no existe 
+                 // podemos declarar una variable del mismo
+    }
+    
+    
+}
