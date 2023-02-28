ab8ae9b46e37fedec01566415a7bbc77fda98c95
Poleth Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2023-02-27 18:04:06 -0600

    Add files via upload

---
+++ EVA-_15_SOBRECARGA/src/eva/_15_sobrecarga/EVA_15_SOBRECARGA.java
@@ -0,0 +1,24 @@
+
+package eva._15_sobrecarga;
+// AUTOR: POLETH E. ESCARCEGA LOPEZ
+public class EVA_15_SOBRECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+       // DIFERENTES PRINTLN: SON 10 DIFERENTES
+       // METODOS PARA IMPRIMIR DU¿IFERENTES TIPOS DE DATOS
+       /* PARA CREAR SOBRECARGA DE MÉTODOS:
+       LA FIRMA 
+       */
+       System.out.println("HOLA MUNDO!!");
+       System.out.println(100);
+       System.out.println(true);
+       System.out.println(args);
+    
+    }
+    
+}
